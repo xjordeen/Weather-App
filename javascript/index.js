4cef9e0d8db6c005cef0ce786a07c0c47a76b290
xjordeen <jordenslater@gmail.com>
2023-09-10 15:55:09 -0700

    Updating the javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,14 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
   let days = [
     "Sunday",
     "Monday",
@@ -28,29 +38,30 @@ function formatDate(date) {
   let currentDay = days[date.getDay()];
   let currentMonth = months[date.getMonth()];
   let currentDate = date.getDate();
-  let hour = date.getHours();
-  let minute = date.getMinutes();
 
-  return `${currentDay}, ${hour}:${minute} 
+  return `${currentDay}, ${hours}:${minutes} 
   <br/> ${currentMonth} ${currentDate}, ${currentYear}
   <br/>`;
 }
 function weatherCondition(response) {
   console.log(response.data);
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#today-temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#feels-like").innerHTML = Math.round(
-    response.data.main.feels_like
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+  let temperatureElement = document.querySelector("#today-temp");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let feelslikeElement = document.querySelector("#feels-like");
+  let dateElement = document.querySelector("#date-time");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  feelslikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
+
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -61,10 +72,6 @@ function submit(event) {
   let city = document.querySelector("#city-input").value;
   searchCity(city);
 }
-
-let dateTime = document.querySelector("#date-time");
-let currentTime = new Date();
-dateTime.innerHTML = formatDate(currentTime);
 
 let form = document.querySelector("#search-bar");
 form.addEventListener("submit", submit);
